ecb2711e6989f063beafb8b302cc5c0837b09809
Fernando Cares <fernandocareswork@gmail.com>
2018-08-01 23:28:10 -0300

    Implementado DELETE de Categorias

--- src/main/java/com/fernandocares/springBoot/services/CategoriaService.java
+++ src/main/java/com/fernandocares/springBoot/services/CategoriaService.java
@@ -3,11 +3,13 @@ package com.fernandocares.springBoot.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.fernandocares.springBoot.domain.Categoria;
 import com.fernandocares.springBoot.repositories.CategoriaRepository;
 import com.fernandocares.springBoot.services.exception.ObjectNotFoundException;
+import com.fernandocares.springBoot.services.exception.DataIntegrityException;
 
 @Service
 public class CategoriaService {
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Falha ao excluir: este registro possui relação com outra tabela");
+		}
 	}
 }
